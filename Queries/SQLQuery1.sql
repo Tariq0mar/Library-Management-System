43ab9f811af3c5c644f1e29aea6bf5c822512bf4
Tariq Omar <tariqomar2004@gmail.com>
2025-05-05 13:34:43 +0300

    more efficient way

--- Queries/SQLQuery1.sql
+++ Queries/SQLQuery1.sql
@@ -4,13 +4,11 @@ DECLARE @BorrowerID INT;
 SET @BorrowerID = 5;  
 
 SELECT 
-    L.LoanID,
-    B.Title,
-    B.Author,
-    B.ISBN,
-    L.DateBorrowed,
-    L.DueDate,
-    L.DateReturned
-FROM Loans L
-JOIN Books B ON L.BookID = B.BookID
-WHERE L.BorrowerID = @BorrowerID;
+    LoanID,
+    BookID,
+    BorrowerID,
+    DateBorrowed,
+    DueDate,
+    DateReturned
+FROM Loans
+WHERE BorrowerID = @BorrowerID;
